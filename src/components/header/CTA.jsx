22128fe0179d410002d47321817723c88117bea4
Martins Ake <akemartins13@gmail.com>
2026-06-08 01:13:42 +0100

    fix hero section: mobile view

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import CV from "../../assets/M-Resume.pdf";
+import { FiDownload } from "react-icons/fi";
 import "./header.css";
 
 const CTA = () => {
   return (
     <div className="cta">
       <a href={CV} download className="btn">
-        Download Resume
+        <FiDownload /> Resume
       </a>
       <a href="#contact" className="btn btn-primary">
         Let's talk
